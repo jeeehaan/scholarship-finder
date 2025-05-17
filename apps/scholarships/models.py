9fd7ca3ac1d04c6af32ea4cf00a7539d41cdc1a0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-17 21:35:47 +0700

    feat(models.py): update Scholarship model to include new fields and use ArrayField for benefits and requirements to enhance data structure and flexibility

--- apps/scholarships/models.py
+++ apps/scholarships/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 from core.models import BaseModel
 
@@ -8,8 +9,13 @@ TYPE_CHOICES = (("full", "Full"), ("partial", "Partial"))
 
 
 class Scholarship(BaseModel):
-    name = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
-    degree_level = models.CharField(max_length=100, choices=DEGREE_LEVEL_CHOICES)
-    end_date = models.DateField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    degree = models.CharField(max_length=100)
+    deadline = models.DateField()
+    registration_start_date = models.DateField()
+    country = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    benefits = ArrayField(models.CharField(max_length=255))
+    requirements = ArrayField(models.CharField(max_length=255))
+    url = models.URLField(unique=True)
