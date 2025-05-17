33ea042632637c89424a13c57fb3d0bf8b996cc0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-17 21:35:00 +0700

    feat(scholarships): enhance ScholarshipListView with context object name and ordering for better data handling feat(scholarships): add TriggerScrapeTaskView to trigger scholarship scraping task via a GET request

--- apps/scholarships/views.py
+++ apps/scholarships/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from apps.scholarships.models import Scholarship
+from django.http import JsonResponse
+from .tasks import scrape_scholarships
+from django.views import View
+
 
 
 # Create your views here.
 class ScholarshipListView(ListView):
     model = Scholarship
+    context_object_name = "scholarships"
+    ordering = ["-created_at"]
     template_name = "dashboard.html"
+
+class TriggerScrapeTaskView(View):
+    def get(self, request, *args, **kwargs):
+        scrape_scholarships()
+        return JsonResponse({"status": "Scholarship fetch task triggered"})
