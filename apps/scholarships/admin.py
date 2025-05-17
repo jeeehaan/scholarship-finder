10007b154b59b6f8d6634c7cad4c3c349e516c2c
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-17 21:35:27 +0700

    feat(admin.py): register Scholarship model in Django admin with custom display fields to enhance admin interface usability

--- apps/scholarships/admin.py
+++ apps/scholarships/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Scholarship
 
 # Register your models here.
+
+
+@admin.register(Scholarship)
+class ScholarshipAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "degree",
+        "deadline",
+        "registration_start_date",
+        "country",
+        "type",
+        "created_at",
+        "url",
+        "benefits",
+        "requirements",
+    )
