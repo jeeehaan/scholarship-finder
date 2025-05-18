9d9610d3a0a3a59644d1c686c0a6e59d19e0cdde
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-18 19:43:42 +0700

    feat(urls.py): add test-query endpoint to enable testing of query functionality in scholarships app

--- apps/scholarships/urls.py
+++ apps/scholarships/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ScholarshipListView, TriggerScrapeTaskView
+from .views import ScholarshipListView, TriggerScrapeTaskView, TestQueryView
 
 urlpatterns = [
     path("dashboard/", ScholarshipListView.as_view(), name="scholarship-list"),
-    path("scrape/", TriggerScrapeTaskView.as_view(), name="trigger-scrape")
+    path("scrape/", TriggerScrapeTaskView.as_view(), name="trigger-scrape"),
+    path("test-query/", TestQueryView.as_view(), name="test-query"),
 ]
