63fea483748dd6643375f996addfa9083fa2cade
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-18 19:42:36 +0700

    feat(methods.py): enhance scholarship scraping and insertion logic with improved error handling and retry mechanism refactor(methods.py): update scholarship data structure and logging for better clarity and maintainability fix(methods.py): correct scholarship URL field name and improve date parsing logic for robustness feat(methods.py): add query_search function to enable searching for scholarships in the database

--- apps/scholarships/methods.py
+++ apps/scholarships/methods.py
@@ -1,8 +1,8 @@
-from crawl4ai import AsyncWebCrawler
+from crawl4ai import AsyncWebCrawler, CacheMode
 from core.ai.chroma import openai_ef
-from core.ai.openai import openai
-from core.ai.structured_output import ScholarshipOutput, ScholarshipList
+from core.ai.structured_output import ScholarshipDetail, ScholarshipList
 from core.ai.prompts import SYSTEM_PROMPT
+from core.ai.prompt_manager import PromptManager
 from crawl4ai.async_configs import CrawlerRunConfig, BrowserConfig, ProxyConfig
 from .models import Scholarship
 from asgiref.sync import sync_to_async
@@ -10,111 +10,164 @@ from dotenv import load_dotenv
 import os
 import logging
 from datetime import datetime
-from chromadb import AsyncHttpClient
+from chromadb import AsyncHttpClient, HttpClient
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 load_dotenv()
 
 logger = logging.getLogger(__name__)
 
 
-proxy_config = ProxyConfig(
-    server=os.getenv("PROXY_SERVER"),
-    username=os.getenv("PROXY_USERNAME"),
-    password=os.getenv("PROXY_PASSWORD"),
-)
-run_config = CrawlerRunConfig(wait_for="css:body")
-
-browser_config = BrowserConfig(proxy_config=proxy_config)
-
 def parse_date(date_str):
-    if not date_str or date_str.strip().lower() in ('', 'n/a', 'na'):
+    if date_str == "N/A" or not date_str:
         return None
+
     try:
-        return datetime.strptime(date_str, '%Y-%m-%d').date()
+        return datetime.strptime(date_str.strip(), "%Y-%m-%d").date()
     except ValueError:
-        try:
-            return datetime.strptime(date_str, '%d/%m/%Y').date()
-        except ValueError:
-            logger.warning(f"Invalid date format: {date_str}")
-            return None
+        logger.error(f"Unexpected date format: {date_str}")
+        return None
+
 
 @sync_to_async
 def scholarship_exists(url):
-    return Scholarship.objects.filter(url=url).exists()
+    return Scholarship.objects.filter(source_url=url).exists()
+
 
 @sync_to_async
 def create_scholarship(data):
     return Scholarship.objects.create(**data)
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 async def scrape_and_insert_scholarships():
+    proxy_config = ProxyConfig(
+        server=os.getenv("PROXY_SERVER"),
+        username=os.getenv("PROXY_USERNAME"),
+        password=os.getenv("PROXY_PASSWORD"),
+    )
+    run_config = CrawlerRunConfig(
+        wait_for="css:body",
+        magic=True,
+        cache_mode=CacheMode.BYPASS,
+        simulate_user=True,
+        override_navigator=True,
+    )
+    browser_config = BrowserConfig(proxy_config=proxy_config)
     client = await AsyncHttpClient(host="localhost", port=8001)
     collection = await client.get_or_create_collection(name="scholarship_finder", embedding_function=openai_ef)
+    p = PromptManager()
+
+    max_limit = 2
     n = 1
     while True:
+        if n == max_limit:
+            break
+
         try:
             async with AsyncWebCrawler() as crawler:
-                result = await crawler.arun(f"https://beasiswa.id/category/beasiswa-magister/page/{n}/", config=run_config)
-                logger.info(f"Scraping page {n}")
-                res = openai.beta.chat.completions.parse(
-                    model="gpt-4.1-mini",
-                    messages=[
+                result = await crawler.arun(
+                    f"https://opportunitiescorners.com/category/scholarships-in-europe/page/{n}/",
+                    config=run_config,
+                )
+                
+                messages=[
                         {"role": "system", "content": SYSTEM_PROMPT},
                         {"role": "user", "content": result.markdown},
-                    ],
-                    response_format=ScholarshipList,
-                )
-                response = res.choices[0].message.parsed
-                '''
-                coba dulu scrape 3 halaman untuk development.
+                ]
                 
-                method yang serius: liat dulu len(response.scholarships), kalau 0 break loop
-                
-                if(len(response.scholarships) == 0):
-                    break
-                '''
-                if(n == 3):
-                    break
-                logger.info(f"Found {len(response.scholarships)} scholarships on page {n}")
-                n += 1
-                
-                for scholarship in response.scholarships:
-                    logger.info(f"Scraping scholarship {scholarship.url}")
-                    try:
-                        async with AsyncWebCrawler() as crawler:
-                            result = await crawler.arun(scholarship.url, config=run_config)
-                            res = openai.beta.chat.completions.parse(
-                                model="gpt-4.1-mini",
-                                messages=[
-                                    {
-                                        "role": "system",
-                                        "content": SYSTEM_PROMPT,
-                                    },
-                                    {"role": "user", "content": result.markdown},
-                                ],
-                                response_format=ScholarshipOutput,
-                            )
-                            scholarship_data = res.choices[0].message.parsed
-                            
-                            if await scholarship_exists(scholarship_data.url):
-                                logger.info(f"Scholarship {scholarship_data.url} already exists, skipping")
-                                continue
-                            
-                            await create_scholarship(scholarship_data.model_dump())
-                            logger.info(f"Created scholarship {scholarship_data.url}")
-                            
-                            await collection.add(
-                                documents=[str(scholarship_data.model_dump())],
-                                ids=[scholarship_data.url],
-                                metadatas=[{"country": scholarship_data.country, "type": scholarship_data.type}],
-                            )
+                p.set_messages(messages)
+                response = p.generate_structured(ScholarshipList)
+                logger.info(f"Found {len(response.scholarships)} scholarships")
 
-                            logger.info(f"Added scholarship {scholarship_data.url} to collection")
+                if len(response.scholarships) == 0:
+                    logger.info("No scholarships found")
+                    continue
+
+                for r in response.scholarships:
+                    if await scholarship_exists(r.url):
+                        logger.info(f"Scholarship {r.url} already exists, skipping")
+                        continue
+                    try:               
+                        async with AsyncWebCrawler() as crawler:
+                            logger.info(f"Scraping {r.url}")
+                            scrape = await crawler.arun(r.url, config=run_config)
+                            messages=[
+                                    {"role": "system", "content": SYSTEM_PROMPT},
+                                    {"role": "user", "content": scrape.markdown},
+                            ]
+                            p.set_messages(messages)
+                            scholarship_data = p.generate_structured(ScholarshipDetail)
+                            
+                            if not scholarship_data:
+                                logger.info(f"Failed to scrape {r.url}, skipping")
+                                continue
+                                
+                            cleaned_data = {
+                                "title": scholarship_data.title,
+                                "description": scholarship_data.description,
+                                "degree": scholarship_data.degree,
+                                "deadline": parse_date(scholarship_data.deadline),
+                                "registration_start_date": parse_date(scholarship_data.registration_start_date),
+                                "country": scholarship_data.country,
+                                "type": scholarship_data.type,
+                                "benefits": scholarship_data.benefits,
+                                "requirements": scholarship_data.requirements,
+                                "official_url": scholarship_data.url,
+                                "source_url": r.url
+                            }
+                                
+                            saved_scholarship = await create_scholarship(cleaned_data)
+                            logger.info(f"Scholarship {cleaned_data['source_url']} successfully inserted to Postgres")
+                            
+                            doc_string = (
+                                    f"Title: {cleaned_data['title']}, "
+                                    f"Description: {cleaned_data['description']}, "
+                                    f"Degree: {cleaned_data['degree']}, "
+                                    f"Deadline: {cleaned_data['deadline']}, "
+                                    f"Registration Start Date: {cleaned_data['registration_start_date']}, "
+                                    f"Country: {cleaned_data['country']}, "
+                                    f"Type: {cleaned_data['type']}, "
+                                    f"Benefits: {', '.join(cleaned_data['benefits']) if cleaned_data['benefits'] else 'None'}, "
+                                    f"Requirements: {', '.join(cleaned_data['requirements']) if cleaned_data['requirements'] else 'None'}, "
+                                    f"Official URL: {cleaned_data['official_url']}"
+                                    f"Source URL: {cleaned_data['source_url']}"
+                            )
+                            await collection.add(
+                                documents=[doc_string],
+                                ids=[saved_scholarship.id],
+                                metadatas=[{"id": saved_scholarship.id, "country": saved_scholarship.country, "type": saved_scholarship.type, "degree": saved_scholarship.degree}],
+                            )
+                            logger.info(f"Scholarship {cleaned_data['source_url']} successfully inserted to Chroma")
+                            
                     except Exception as e:
-                        logger.warning(f"Error parsing date for scholarship {scholarship.url}: {e}")
+                        logger.error(f"Failed to process {r.url}: {e}")
+                        continue
+                n += 1
         except Exception as e:
-            logger.error(f"Error on page {n}: {e}")
-            break
-        
-           
-              
+            logger.error(f"Failed to process page {n}: {e}")
+            continue
+
+
+
+def query_search(query):
+    client = HttpClient(host="localhost", port=8001)
+    collection = client.get_collection(name="scholarship_finder", embedding_function=openai_ef)
+    results = collection.query(
+        query_texts=[query],
+        n_results=3,
+    )
+    search_results = []
+    for metadata in results["metadatas"][0]:
+        scholarship = Scholarship.objects.get(id=metadata["id"])
+        search_results.append(
+            {
+                "id": scholarship.id,
+                "title": scholarship.title,
+                "degree": scholarship.degree,
+                "deadline": scholarship.deadline,
+                "country": scholarship.country,
+                "type": scholarship.type,
+            }
+        )
+    return search_results
