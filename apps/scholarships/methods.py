b497ad031c55f1047d5e76aa29ce3694de16b0fb
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-20 00:16:20 +0700

    feat(method.py): implement new structure output and fix result checking

--- apps/scholarships/methods.py
+++ apps/scholarships/methods.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from chromadb import AsyncHttpClient, HttpClient
 from tenacity import retry, stop_after_attempt, wait_fixed
 
-load_dotenv()
+load_dotenv(override=True)
 
 logger = logging.getLogger(__name__)
 
@@ -38,7 +38,6 @@ def scholarship_exists(url):
 def create_scholarship(data):
     return Scholarship.objects.create(**data)
 
-
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 async def scrape_and_insert_scholarships():
     proxy_config = ProxyConfig(
@@ -55,11 +54,13 @@ async def scrape_and_insert_scholarships():
     )
     browser_config = BrowserConfig(proxy_config=proxy_config)
     client = await AsyncHttpClient(host="localhost", port=8001)
-    collection = await client.get_or_create_collection(name="scholarship_finder", embedding_function=openai_ef)
+    collection = await client.get_or_create_collection(
+        name="scholarship_finder", embedding_function=openai_ef
+    )
     p = PromptManager()
 
-    max_limit = 2
-    n = 1
+    max_limit = 7
+    n = 6
     while True:
         if n == max_limit:
             break
@@ -71,11 +72,15 @@ async def scrape_and_insert_scholarships():
                     config=run_config,
                 )
                 
-                messages=[
-                        {"role": "system", "content": SYSTEM_PROMPT},
-                        {"role": "user", "content": result.markdown},
+                if(not result.markdown):
+                    logger.info(f"Failed to scrape page {n}, skipping")
+                    continue
+
+                messages = [
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content": result.markdown},
                 ]
-                
+
                 p.set_messages(messages)
                 response = p.generate_structured(ScholarshipList)
                 logger.info(f"Found {len(response.scholarships)} scholarships")
@@ -88,58 +93,78 @@ async def scrape_and_insert_scholarships():
                     if await scholarship_exists(r.url):
                         logger.info(f"Scholarship {r.url} already exists, skipping")
                         continue
-                    try:               
+                    try:
                         async with AsyncWebCrawler() as crawler:
                             logger.info(f"Scraping {r.url}")
                             scrape = await crawler.arun(r.url, config=run_config)
-                            messages=[
-                                    {"role": "system", "content": SYSTEM_PROMPT},
-                                    {"role": "user", "content": scrape.markdown},
+                            
+                            if(not scrape.markdown):
+                                logger.info(f"Failed to scrape {r.url}, skipping")
+                                continue
+                            
+                            messages = [
+                                {"role": "system", "content": SYSTEM_PROMPT},
+                                {"role": "user", "content": scrape.markdown},
                             ]
                             p.set_messages(messages)
                             scholarship_data = p.generate_structured(ScholarshipDetail)
-                            
-                            if not scholarship_data:
+                            logger.info(f"scholarship_data: {scholarship_data}")
+
+                            if scholarship_data.title == "N/A":
                                 logger.info(f"Failed to scrape {r.url}, skipping")
                                 continue
-                                
+
                             cleaned_data = {
                                 "title": scholarship_data.title,
                                 "description": scholarship_data.description,
                                 "degree": scholarship_data.degree,
                                 "deadline": parse_date(scholarship_data.deadline),
-                                "registration_start_date": parse_date(scholarship_data.registration_start_date),
+                                "registration_start_date": parse_date(
+                                    scholarship_data.registration_start_date
+                                ),
                                 "country": scholarship_data.country,
                                 "type": scholarship_data.type,
                                 "benefits": scholarship_data.benefits,
                                 "requirements": scholarship_data.requirements,
                                 "official_url": scholarship_data.url,
-                                "source_url": r.url
+                                "source_url": r.url,
+                                "must_relocate": scholarship_data.must_relocate,
+                                "study_format": scholarship_data.study_format
                             }
-                                
-                            saved_scholarship = await create_scholarship(cleaned_data)
-                            logger.info(f"Scholarship {cleaned_data['source_url']} successfully inserted to Postgres")
                             
+                            saved_scholarship = await create_scholarship(cleaned_data)
+                            logger.info(
+                                f"Scholarship {cleaned_data['source_url']} successfully inserted to Postgres"
+                            )
+
                             doc_string = (
-                                    f"Title: {cleaned_data['title']}, "
-                                    f"Description: {cleaned_data['description']}, "
-                                    f"Degree: {cleaned_data['degree']}, "
-                                    f"Deadline: {cleaned_data['deadline']}, "
-                                    f"Registration Start Date: {cleaned_data['registration_start_date']}, "
-                                    f"Country: {cleaned_data['country']}, "
-                                    f"Type: {cleaned_data['type']}, "
-                                    f"Benefits: {', '.join(cleaned_data['benefits']) if cleaned_data['benefits'] else 'None'}, "
-                                    f"Requirements: {', '.join(cleaned_data['requirements']) if cleaned_data['requirements'] else 'None'}, "
-                                    f"Official URL: {cleaned_data['official_url']}"
-                                    f"Source URL: {cleaned_data['source_url']}"
+                                f"Title: {cleaned_data['title']}, "
+                                f"Description: {cleaned_data['description']}, "
+                                f"Degree: {cleaned_data['degree']}, "
+                                f"Deadline: {cleaned_data['deadline']}, "
+                                f"Registration Start Date: {cleaned_data['registration_start_date']}, "
+                                f"Country: {cleaned_data['country']}, "
+                                f"Type: {cleaned_data['type']}, "
+                                f"Benefits: {', '.join(cleaned_data['benefits']) if cleaned_data['benefits'] else 'None'}, "
+                                f"Requirements: {', '.join(cleaned_data['requirements']) if cleaned_data['requirements'] else 'None'}, "
+                                f"Official URL: {cleaned_data['official_url']}"
+                                f"Source URL: {cleaned_data['source_url']}"
+                                f"Must Relocate: {cleaned_data['must_relocate']}"
+                                f"Study Format: {cleaned_data['study_format']}"
                             )
                             await collection.add(
                                 documents=[doc_string],
                                 ids=[saved_scholarship.id],
-                                metadatas=[{"id": saved_scholarship.id, "country": saved_scholarship.country, "type": saved_scholarship.type, "degree": saved_scholarship.degree}],
+                                metadatas=[
+                                    {
+                                        "id": saved_scholarship.id,
+                                    }
+                                ],
                             )
-                            logger.info(f"Scholarship {cleaned_data['source_url']} successfully inserted to Chroma")
-                            
+                            logger.info(
+                                f"Scholarship {cleaned_data['source_url']} successfully inserted to Chroma"
+                            )
+
                     except Exception as e:
                         logger.error(f"Failed to process {r.url}: {e}")
                         continue
@@ -149,10 +174,12 @@ async def scrape_and_insert_scholarships():
             continue
 
 
-
 def query_search(query):
     client = HttpClient(host="localhost", port=8001)
-    collection = client.get_collection(name="scholarship_finder", embedding_function=openai_ef)
+    collection = client.get_collection(
+        name="scholarship_finder", embedding_function=openai_ef
+    )
+    print("count data in collection", collection.count())
     results = collection.query(
         query_texts=[query],
         n_results=3,
