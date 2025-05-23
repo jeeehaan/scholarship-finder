1ba5f35649a3aec4a39aa515bce61c1009028a19
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-23 22:11:18 +0700

    feat(models.py): add ScholarshipRecommendation model to link scholarships with users for better tracking of recommendations refactor(models.py): remove unused degree level and type choices to clean up the codebase

--- apps/scholarships/models.py
+++ apps/scholarships/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
 from core.models import BaseModel
-
-DEGREE_LEVEL_CHOICES = (("bachelor", "Bachelor"), ("master", "Master"))
-
-TYPE_CHOICES = (("full", "Full"), ("partial", "Partial"))
-
 
 class Scholarship(BaseModel):
     title = models.CharField(max_length=255)
@@ -22,4 +18,9 @@ class Scholarship(BaseModel):
     source_url = models.URLField(unique=True, blank=True, null=True)
     must_relocate = models.BooleanField(default=False)
     study_format = models.CharField(max_length=100, blank=True, null=True)
+    
+    
+class ScholarshipRecommendation(BaseModel):
+    scholarship = models.ForeignKey(Scholarship, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
