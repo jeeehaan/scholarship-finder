85f35062388a482279d96e7df395bb60a53bb102
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-23 22:11:38 +0700

    refactor(views.py): replace ListView with TemplateView for ScholarshipListView to customize context data and improve scholarship recommendations handling chore(views.py): clean up commented-out code for better readability and maintainability

--- apps/scholarships/views.py
+++ apps/scholarships/views.py
@@ -1,21 +1,67 @@
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 from django.http import JsonResponse
 from .tasks import scrape_scholarships
 from django.views import View
-from .models import Scholarship
+from .models import ScholarshipRecommendation
 from .methods import query_search
 
-class ScholarshipListView(ListView):
-    model = Scholarship
-    context_object_name = "scholarships"
-    ordering = ["-created_at"]
+# class ScholarshipListView(ListView):
+#     model = Scholarship
+#     context_object_name = "scholarships"
+#     ordering = ["-created_at"]
+#     template_name = "dashboard.html"
+
+
+class ScholarshipListView(TemplateView):
     template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        scholarship_recommendations = ScholarshipRecommendation.objects.filter(
+            user=user
+        )
+        
+        result = []
+        for recommendation in scholarship_recommendations:
+            scholarship = recommendation.scholarship
+            result.append(
+                {
+                    "title": scholarship.title,
+                    "degree": scholarship.degree,
+                    "country": scholarship.country,
+                    "type": scholarship.type,
+                    "deadline": scholarship.deadline
+                }
+            )
+        context["scholarships"] = result
+        return context
+
+    # def get(self, request, *args, **kwargs):
+    #     user = self.request.user
+    #     scholarship_recommendations = ScholarshipRecommendation.objects.filter(
+    #         user=user
+    #     )
+    #     return JsonResponse(
+    #         {
+    #             "scholarship_recommendations": [
+    #                 {
+    #                     "title": scholarship.scholarship.title,
+    #                     "degree": scholarship.scholarship.degree,
+    #                     "country": scholarship.scholarship.country,
+    #                     "type": scholarship.scholarship.type,
+    #                 }
+    #                 for scholarship in scholarship_recommendations
+    #             ]
+    #         }
+    #     )
+
 
 class TriggerScrapeTaskView(View):
     def get(self, request, *args, **kwargs):
         scrape_scholarships()
         return JsonResponse({"status": "Scholarship fetch task triggered"})
-    
+
 
 class TestQueryView(View):
     def get(self, request, *args, **kwargs):
