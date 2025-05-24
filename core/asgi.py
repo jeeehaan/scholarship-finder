b38d0e5c09c563cbcd751b7c0d66f3d9f4f76c2c
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-24 20:42:30 +0700

    feat(asgi.py): implement ASGI ProtocolTypeRouter to support WebSocket connections alongside HTTP requests

--- core/asgi.py
+++ core/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+from .urls import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
