22065f4a156e5acdd7eb5d375b539c95c1ef2191
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-24 20:48:20 +0700

    feat(chroma.py): integrate HttpClient from chromadb and load environment variables to enhance configuration management

--- core/ai/chroma.py
+++ core/ai/chroma.py
@@ -1,9 +1,11 @@
-
+from chromadb import HttpClient
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 import os
+from dotenv import load_dotenv
 
-# chroma = AsyncHttpClient(host="localhost", port=8001)
+load_dotenv()
 
+chroma_client = HttpClient(host="localhost", port=5010)
 openai_ef = OpenAIEmbeddingFunction(
     model_name="text-embedding-3-large",
     api_key=os.getenv("OPENAI_API_KEY"),
