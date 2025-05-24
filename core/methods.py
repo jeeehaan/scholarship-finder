b8a1b42bf8e5fb41ed34085f8d2b01d34794ff17
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-24 20:39:01 +0700

    feat(methods.py): add notification and chat message sending methods to handle real-time communication

---
+++ core/methods.py
@@ -0,0 +1,25 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+
+def send_notification(notification_type, content):
+    channel = get_channel_layer()
+    async_to_sync(channel.group_send)(
+        "notification",
+        {
+            "type": "send_notification",
+            "message": {"content": content, "type": notification_type},
+        },
+    )
+
+
+def send_chat_message(message, scholarships):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        "chat",
+        {
+            "type": "send_message",
+            "message": message,
+            "scholarships": scholarships
+        },
+    )
