a490897ef4711883ba1eb25736dfee93e1297aa0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-24 20:41:02 +0700

    feat(urls.py): add websocket URL pattern for chat functionality and static file serving in debug mode
    This change introduces a new URL pattern for WebSocket connections
    to enable real-time chat features and ensures that static files are
    served correctly when the application is in debug mode.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index_view
+
+from .consumer import ChatConsumer
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +12,10 @@ urlpatterns = [
     path("", include("apps.scholarships.urls")),
     path("", include("apps.authentication.urls")),
 ]
+
+websocket_urlpatterns = [
+    path("ws/chat/", ChatConsumer.as_asgi()),
+]
+
+if settings.DEBUG:
+  urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
