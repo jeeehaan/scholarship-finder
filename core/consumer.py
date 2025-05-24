cadcddebf19db90db7d30d4dd831818095422e93
Nadia Puji Utami <nadiapujiutami@gmail.com>
2025-05-24 20:38:47 +0700

    feat(consumer.py): add WebSocket consumers for notifications and chat to handle real-time messaging functionality

---
+++ core/consumer.py
@@ -0,0 +1,42 @@
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from apps.chat.tasks import process_chat
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add("notification", self.channel_name)
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("notification", self.channel_name)
+
+    async def send_notification(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"message": message}))
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add("chat", self.channel_name)
+       
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("chat", self.channel_name)
+
+    async def receive(self, text_data):
+        user = self.scope['user']
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get("message")
+
+        process_chat(message, user)
+
+
+
+    async def send_message(self, event):
+        message = event["message"]
+        scholarships = event["scholarships"]
+        await self.send(text_data=json.dumps({"message": message, "scholarships": scholarships}))
